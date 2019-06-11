e040c3cd209ac2b93ad1051f1091689e1ea48d9f
punkkk <punkrus18@gmail.com>
2019-06-11 20:37:50 +0300

    Base three screens for new game also import screen loader from lizard project

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    public void loadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void failScreen()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
