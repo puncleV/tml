47715b6370b776386bf1a67aaa6f12240b8519ef
punkkk <punkrus18@gmail.com>
2019-06-12 20:48:12 +0300

    move paddle with mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] int units;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.move();
+    }
+
+    private void move()
+    {
+        float newX = Input.mousePosition.x / Screen.width * units;
+        Vector2 newPosition = new Vector2(newX, transform.position.y);
+        transform.position = newPosition;
+    }
+}
