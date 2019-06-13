c9253741627ab3e3aa4ccc38d2337308e8844a0e
punkkk <punkrus18@gmail.com>
2019-06-13 19:36:11 +0300

    restrict screen overflow to paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] int units;
+    [SerializeField] float maxX;
+    [SerializeField] float minX;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -20,7 +21,14 @@ public class Paddle : MonoBehaviour
     private void move()
     {
         float newX = Input.mousePosition.x / Screen.width * units;
-        Vector2 newPosition = new Vector2(newX, transform.position.y);
-        transform.position = newPosition;
+
+        transform.position = this.getNewXPosition(newX);
+    }
+
+    private Vector2 getNewXPosition(float newX)
+    {
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        currentPosition.x = Mathf.Clamp(newX, minX, maxX);
+        return currentPosition;
     }
 }
