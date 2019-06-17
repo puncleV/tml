3d0bc5ecf83b0cc8d7b10f36f3c86b5b3086c25a
punkkk <punkrus18@gmail.com>
2019-06-17 19:33:00 +0300

    destroy block on collision

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] int health = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(--health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
