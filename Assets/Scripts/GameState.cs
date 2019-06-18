e54429f25cfb75fc2eb382ed01d6ec3e03f9a153
punkkk <punkrus18@gmail.com>
2019-06-18 20:19:51 +0300

    add game state class with tame scaling setting

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    [SerializeField] float gameSpeedScale = 1;
+    [SerializeField] float speedChangeStep = 0.01f;
+
+    private void Start()
+    {
+        Time.timeScale = gameSpeedScale;
+    }
+
+    public void onBlockDestroied()
+    {
+        increaseSpeed();
+    }
+
+    private void increaseSpeed()
+    {
+        gameSpeedScale += speedChangeStep;
+        Time.timeScale = gameSpeedScale;
+    }
+}
