3fd0cf77128f69aa20a4f31bb17534ecb23e64d7
punkkk <punkrus18@gmail.com>
2019-06-18 19:27:24 +0300

    add sounds on block destruction

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] int health = 1;
+    [SerializeField] AudioClip destroySound;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(--health == 0)
         {
+            AudioSource.PlayClipAtPoint(destroySound, transform.position);
             Destroy(gameObject);
         }
     }
