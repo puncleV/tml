de61264ca7a21cf80263a521fdbb75795d3e6a52
punkkk <punkrus18@gmail.com>
2019-06-20 19:49:58 +0300

    little prettify code

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -8,8 +8,14 @@ public class Paddle : MonoBehaviour
     [SerializeField] float maxX;
     [SerializeField] float minX;
     // Start is called before the first frame update
+
+    GameState gameState;
+    Ball ball;
+
     void Start()
     {
+        gameState = FindObjectOfType<GameState>();
+        ball = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
@@ -20,21 +26,16 @@ public class Paddle : MonoBehaviour
 
     private void move()
     {
-        float newX = getXPos();
-        Vector2 newPosition = new Vector2(transform.position.x, transform.position.y);
-        newPosition.x = Mathf.Clamp(newX, minX, maxX);
+        Vector2 newPosition = new Vector2(Mathf.Clamp(getXPos(), minX, maxX), transform.position.y);
         transform.position = newPosition;
     }
 
     private float getXPos()
     {
-        bool autoplayOn = FindObjectOfType<GameState>().isAutoPlay();
-        Debug.Log(autoplayOn);
+        bool autoplayOn = gameState.isAutoPlay();
         if(autoplayOn)
         {
-            Debug.Log(FindObjectOfType<Ball>().transform.position.x);
-
-            return FindObjectOfType<Ball>().transform.position.x;
+            return ball.transform.position.x;
         } else
         {
             return Input.mousePosition.x / Screen.width * units;
